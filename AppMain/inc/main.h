c341e17fd4422fa4d68b953dc374e3e90b01dba1
JFogotcheck <fogotcheck@live.com>
2025-02-01 13:44:53 +0300

    build: add pre-commit

--- AppMain/inc/main.h
+++ AppMain/inc/main.h
@@ -4,4 +4,4 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "event_groups.h"
-#include "cmsis_os2.h" 
+#include "cmsis_os2.h"
--- AppMain/src/main.cpp
+++ AppMain/src/main.cpp
@@ -4,25 +4,22 @@ void MainThr(__attribute__((unused)) void *arg);
 
 int main(void)
 {
-    system_init();
-    BaseType_t ret =
-        xTaskCreate(MainThr, "MainTask", 500, NULL, 1, NULL);
-    if (ret != pdPASS)
-    {
-        Error_Handler();
-    }
-    vTaskStartScheduler();
-    Error_Handler();
+	system_init();
+	BaseType_t ret = xTaskCreate(MainThr, "MainTask", 500, NULL, 1, NULL);
+	if (ret != pdPASS) {
+		Error_Handler();
+	}
+	vTaskStartScheduler();
+	Error_Handler();
 }
 
 void MainThr(__attribute__((unused)) void *arg)
 {
-    if (osKernelInitialize()) {
+	if (osKernelInitialize()) {
 		Error_Handler();
 	}
-    while (1)
-    {
-        HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
-        vTaskDelay(1000);
-    }
+	while (1) {
+		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+		vTaskDelay(1000);
+	}
 }
