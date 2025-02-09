c9eb0bb6f25086035c31f33b9d580d4829eaa0d3
JFogotcheck <fogotcheck@live.com>
2025-02-09 17:22:57 +0300

    fix: rm queue, dma->poling mode

--- AppMain/src/main.cpp
+++ AppMain/src/main.cpp
@@ -1,9 +1,10 @@
 #include "main.h"
 #include "usart.h"
 #include "Logger.h"
-
+#include <stdio.h>
 void MainThr(__attribute__((unused)) void *arg);
-
+void TestThr(void *arg);
+Logger *pLog = nullptr;
 int main(void)
 {
 	system_init();
@@ -22,10 +23,40 @@ void MainThr(__attribute__((unused)) void *arg)
 	}
 
 	LoggerPortUsart loggerPort_usart;
-
 	Logger log(&loggerPort_usart);
+	pLog = &log;
+	vTaskDelay(1000);
+	for (uint32_t i = 0; i < 20; i++) {
+		BaseType_t ret = xTaskCreate(TestThr, "TestTask", 256,
+					     (void *)i, (i + 1), NULL);
+		if (ret != pdPASS) {
+			Error_Handler();
+		}
+	}
+
 	while (1) {
 		HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
 		vTaskDelay(1000);
+	}
+}
+
+void TestThr(void *arg)
+{
+	uint32_t i = (uint32_t)arg;
+
+	char tmp[256] = { 0 };
+	char name[16] = { 0 };
+	sprintf((char *)name, "%s::%lu::\t", __func__, i);
+	sprintf((char *)tmp,
+		"mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_\r\n");
+
+	while (1) {
+		if (pLog) {
+			if (pLog->AddMSG(name, (uint8_t *)tmp, strlen(tmp))) {
+				Error_Handler();
+			}
+		}
+
+		vTaskDelay(500);
 	}
 }
--- Lib/Logger/kernel/src/Logger.cpp
+++ Lib/Logger/kernel/src/Logger.cpp
@@ -9,15 +9,6 @@ Logger::Logger(LoggerPort *p_port)
 		Error_Handler();
 	}
 
-	handle.rtos.xQueue =
-		xQueueCreate(LOGGER_QUEUE_ITEM_SIZE, sizeof(LoggerMSG_t));
-	if (handle.rtos.xQueue == nullptr) {
-		Error_Handler();
-	}
-
-	strcat(handle.name, LOGGER_TASK_NAME);
-	strcat(handle.name, "::\t");
-
 	BaseType_t ret = xTaskCreate(
 		[](void *arg) { static_cast<Logger *>(arg)->LoggerThr(arg); },
 		LOGGER_TASK_NAME, LOGGER_STACK_SIZE, this, LOGGER_PRIORITY,
@@ -31,10 +22,9 @@ Logger::~Logger()
 {
 	vTaskDelete(handle.rtos.xTask);
 	vSemaphoreDelete(handle.rtos.xSemaphore);
-	vQueueDelete(handle.rtos.xQueue);
 }
 
-int32_t Logger::AddMSG(char *ModuleName, const uint8_t *Data, uint16_t Size)
+int32_t Logger::AddMSG(char *ModuleName, uint8_t *Data, uint16_t Size)
 {
 	if (ModuleName == nullptr || Data == nullptr || Size == 0) {
 		return -1;
@@ -44,72 +34,39 @@ int32_t Logger::AddMSG(char *ModuleName, const uint8_t *Data, uint16_t Size)
 		return -1;
 	}
 
-	strncpy(handle.name, ModuleName, LOGGER_MODULE_SIZE_NAME);
-	handle.name[LOGGER_MODULE_SIZE_NAME] = '\0';
+	msg.name = (uint8_t *)ModuleName;
+	msg.data = Data;
+	msg.size = Size;
+	xTaskNotifyGive(handle.rtos.xTask);
 
-	uint8_t NameSize = strlen(handle.name);
-	uint16_t availableDataSize = LOGGER_DATA_MSG_SIZE - NameSize;
-
-	LoggerMSG_t msg = { 0 };
-
-	memcpy(msg.data, handle.name, NameSize);
-
-	uint16_t dataSizeToCopy =
-		(Size < availableDataSize) ? Size : availableDataSize;
-	memcpy(msg.data + NameSize, Data, dataSizeToCopy);
-	msg.size = NameSize + dataSizeToCopy;
-
-	QueueAddMSG(&msg);
-
-	uint16_t offset = dataSizeToCopy;
-	while (offset < Size) {
-		msg = { 0 };
-
-		uint16_t remainingDataSize = Size - offset;
-		dataSizeToCopy = (remainingDataSize < availableDataSize) ?
-					 remainingDataSize :
-					 availableDataSize;
-
-		memcpy(msg.data, Data + offset, dataSizeToCopy);
-		msg.size = dataSizeToCopy;
-
-		QueueAddMSG(&msg);
-
-		offset += dataSizeToCopy;
-	}
 	xSemaphoreGive(handle.rtos.xSemaphore);
 
 	return 0;
 }
 
-void Logger::QueueAddMSG(LoggerMSG_t *msg)
-{
-	BaseType_t xStatus = xQueueSend(handle.rtos.xQueue, msg, 10);
-	if (xStatus != pdPASS) {
-		vTaskDelay(10);
-		xStatus = xQueueSend(handle.rtos.xQueue, msg, 10);
-		if (xStatus != pdPASS) {
-			xStatus = xQueueOverwrite(handle.rtos.xQueue, msg);
-		}
-	}
-}
-
 void Logger::LoggerThr(void *arg)
 {
 	Logger *log = static_cast<Logger *>(arg);
-	static LoggerMSG_t msg = { 0 };
 
-	uint8_t tmp[] =
-		"mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_mama_ Init::\tOK\r\n";
-	if (log->AddMSG(log->handle.name, tmp, sizeof(tmp))) {
-		vTaskDelete(log->handle.rtos.xTask);
+	if (log->handle.port->write((uint8_t *)log->handle.name,
+				    strlen(log->handle.name))) {
+		Error_Handler();
+	}
+
+	static uint8_t tmp[] = "Init::\tOK\r\n";
+	if (log->handle.port->write(tmp, sizeof(tmp))) {
+		log->~Logger();
 	}
 
 	while (1) {
-		if (xQueueReceive(log->handle.rtos.xQueue, &msg,
-				  portMAX_DELAY) == pdPASS) {
-			log->handle.port->write(msg.data, msg.size);
-			vTaskDelay(1000);
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		if (log->handle.port->write((uint8_t *)log->msg.name,
+					    strlen((char *)log->msg.name))) {
+			Error_Handler();
+		}
+
+		if (log->handle.port->write(log->msg.data, log->msg.size)) {
+			Error_Handler();
 		}
 	}
 }
